45b8e53cef5484b3c231bbd9146c5413657f2a7e
Pieter Deane <deanepieter@gmail.com>
2023-11-12 16:36:09 +0200

    added some lazy loading skeletons implemented dashboard overview as a route group

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Image from 'next/image';
 
 
 import Link from 'next/link';
-import { Lusitana } from 'next/font/google';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
